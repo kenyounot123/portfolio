a42f6e36934eee00a697b5a0d61480698466bab2
kenyounot123 <kenlu519@gmail.com>
2024-07-11 15:24:23 -0400

    Create next.js app, add nextui and nexthemes libraries to implement dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <body className={inter.className}>
+        <ThemeProvider enableSystem={false}>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
